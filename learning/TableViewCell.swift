f2023021a51ae37dd082f451e083f8f05f5dd9aa
saad <saadneosoft@gmail.com>
2023-07-20 12:39:02 +0530

    basicview

---
+++ learning/TableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  TableViewCell.swift
+//  learning
+//
+//  Created by Neosoft on 20/07/23.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var gettingNUmber: UILabel!
+    
+    @IBOutlet weak var gettingtopics: UILabel!
+    
+}
--- learning/ViewController.swift
+++ learning/ViewController.swift
@@ -7,13 +7,46 @@
 
 import UIKit
 
+struct way{
+    var num = Int()
+    var name = String()
+}
 class ViewController: UIViewController {
-
+    
+    var detials = [
+        way(num: 1, name: "UiSegment") ,
+        way(num: 2, name: "UiSwitch")
+    ]
+    
+    @IBOutlet weak var tableview: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension ViewController : UITableViewDelegate,UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return detials.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        var cnt =  detials[indexPath.row]
+        cell.gettingtopics.text = cnt.name
+        cell.gettingNUmber.text = String(cnt.num)
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var cnt =  detials[indexPath.row]
+        var UiSegment = storyboard?.instantiateViewController(withIdentifier: "SecViewController") as! SecViewController
+        var Uiswitch = storyboard?.instantiateViewController(withIdentifier: "thirdViewController") as! thirdViewController
+        if cnt.name == "UiSegment"{
+            navigationController?.pushViewController(UiSegment, animated: true)
+        }
+        else if (cnt.name == "UiSwitch"){
+            navigationController?.pushViewController(Uiswitch, animated: true)
+        }
+    }
+}
